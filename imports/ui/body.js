581e7ddbcf451de4ca27caf24fab14e57f1f18ad
Paul Estrada <paul_e_mtz@hotmail.com>
2016-08-19 17:00:38 -0500

    fix issues with collapsing menu

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,10 +1,8 @@
- import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Projects } from '../api/documents.js';
 import './document.js'
 import './body.html';
-
-
 
 
 Template.editor.onCreated(function bodyOnCreated() {
@@ -14,23 +12,24 @@ Template.editor.helpers({
   projects() {
     return Projects.find({}).fetch();
     },
-
 });
 
-  Template.editor.events({
-    'click .view' (event){
-      var url ='http://localhost:3000/previewview';
-      window.open(url, '_blank');
-     }
-  });
+Template.editor.events({
+  'click .view' (event){
+    var url ='http://localhost:3000/previewview';
+    window.open(url, '_blank');
+   }
+});
 
-
-
-  Template.editor.events({
-    'click #menuToggler' (event){
-  $('#wrapper').toggleClass('toggled');
-     }
-  });
+Template.editor.events({
+  'click #menuToggler' (event){
+    $('.collapse').collapse('toggle');
+    var wrapper = $('#wrapper');
+    if (!wrapper.hasClass('toggled')) {
+      wrapper.toggleClass('toggled');
+    }
+  }
+});
 
 var readLines = function (docs){
   var num = 0;
@@ -61,79 +60,78 @@ var searchs = function (docs, filename){
     }
   }
 
- var extension=filename.split(".")[1];
-
-    if(extension=="html"){
-       var editor = $('.CodeMirror')[0].CodeMirror;
-       editor.setOption("mode","text/html");    
-    }else if(extension=="css"){
-       var editor = $('.CodeMirror')[0].CodeMirror;
-       editor.setOption("mode","text/css");    
-    }else if(extension=="js"){
-       var editor = $('.CodeMirror')[0].CodeMirror;
-       editor.setOption("mode","text/javascript");  
-    }else if(extension=="php"){
-       var editor = $('.CodeMirror')[0].CodeMirror;
-       editor.setOption("mode","text/x-php"); 
-    }
+  var extension=filename.split(".")[1];
+  if(extension=="html"){
+     var editor = $('.CodeMirror')[0].CodeMirror;
+     editor.setOption("mode","text/html");    
+  }else if(extension=="css"){
+     var editor = $('.CodeMirror')[0].CodeMirror;
+     editor.setOption("mode","text/css");    
+  }else if(extension=="js"){
+     var editor = $('.CodeMirror')[0].CodeMirror;
+     editor.setOption("mode","text/javascript");  
+  }else if(extension=="php"){
+     var editor = $('.CodeMirror')[0].CodeMirror;
+     editor.setOption("mode","text/x-php"); 
+  }
   return result;
-
 }
-var save = function (){
-      var editor = $('.CodeMirror')[0].CodeMirror;
-      var data = editor.getValue("\n"); 
-      var arrayData= data.split("\n");
-      var projectId = $(".save").attr("data-projectId"); 
-      var arrayMongo =[];
 
+var save = function (){
+  var editor = $('.CodeMirror')[0].CodeMirror;
+  var data = editor.getValue("\n"); 
+  var arrayData= data.split("\n");
+  var projectId = $(".save").attr("data-projectId"); 
+  var arrayMongo =[];
+
+  var result;
   for (var i=0; i<arrayData.length; i++) {
     result = { 
       text: arrayData[i]
     };
     arrayMongo[i] = result;
- }
+  }
 
 
-var index= $("#editorcode").attr("data-currentFile");
-if (index ==""){
-  alert("Seleccione un archivo");
-}else{
+  var index= $("#editorcode").attr("data-currentFile");
+  if (index ==""){
+    alert("Seleccione un archivo");
+  } else {
+    var setModifier = { $set: {} };
+    setModifier.$set['files.'+index+'.lines'] = arrayMongo;
 
-
-var setModifier = { $set: {} };
-setModifier.$set['files.'+index+'.lines'] = arrayMongo;
-
-Projects.update(new Mongo.ObjectID(projectId), setModifier);
-  console.log("\"Guardado\"");
-}
+    Projects.update(new Mongo.ObjectID(projectId), setModifier);
+    console.log("\"Guardado\"");
+  }
 }
 
-  Template.editor.events({
-    'click .save' (event){
-      save();
-     }
-  });
+Template.editor.events({
+  'click .save' (event){
+    save();
+   }
+});
 
-  Template.editor.events({
-    'click .records' (event){
+Template.editor.events({
+  'click .records' (event){
     var numberIndex = $(event.target).attr("data-recorId");
     var currentFile= $("#editorcode").attr("data-currentFile",numberIndex);
     var filename= event.target.innerText;
     var tu=document.getElementById('valores').innerHTML =" "+filename;
     Meteor.call('project.find',(err, res) => {
-    if (err) {
-      alert(err);
-    } else {
-    var text = searchs(res, filename);
-    var editor = $('.CodeMirror')[0].CodeMirror;
-        editor.setValue(text);   
+      if (err) {
+        alert(err);
+      } else {
+        var text = searchs(res, filename);
+        var editor = $('.CodeMirror')[0].CodeMirror;
+        editor.setValue(text);
+        var menu = $(".collapse");
+        if (menu.hasClass('in')) {
+          $(".collapse").collapse('toggle');
         }
-     });
-    }
- });
-
-
-
+      }
+    });
+  }
+});
 
 Template.editor.onRendered( function() {
   this.editor = CodeMirror.fromTextArea( this.find( "#editorcode" ), {
@@ -150,8 +148,6 @@ Template.editor.onRendered( function() {
      const isReady = subscriptions.ready();
      var docs = Projects.find({});
      if (isReady && docs) {
-    // var lines = readLines(docs)
-
       $(".save").attr("data-projectId",docs.fetch()[0]._id);
      }
   });
