8250c55c699fbc8b69bdfc44cdb3d5886d9524e6
Paul Estrada <paul_e_mtz@hotmail.com>
2016-08-22 15:30:45 -0500

    formatting code

--- imports/ui/instructor.js
+++ imports/ui/instructor.js
@@ -5,7 +5,6 @@ import './document.js'
 import { Editor } from './editor.js'
 import './instructor.html';
 
-
 Template.instructor.onCreated(function bodyOnCreated() {
 });
 
@@ -14,65 +13,29 @@ Template.instructor.helpers({
     return Instructor.find({}).fetch();
     },
 });
-  
-var searchs = function (docs,filename){
-  var files = docs.files;
-  var result="";
-  for (var i=0; i<files.length; i++) {  
-    var name= docs.files[i].name;
-    var extension= docs.files[i].extension;
-    var nameExtencion = name+"."+extension;
-    if (filename==nameExtencion) {
-     var lines = docs.files[i].lines;
-       for (var j=0; j<lines.length; j++) {
-        result = result+lines[j].text+"\n";
-      }
 
-    }
-  }
-
-    var extension=filename.split(".")[1];
-   if(extension=="css"){
-           var editor = $('.CodeMirror')[0].CodeMirror;
-           editor.setOption("mode","text/css");    
-        }else if(extension=="js"){
-           var editor = $('.CodeMirror')[0].CodeMirror;
-           editor.setOption("mode","text/javascript");  
-        }else if(extension=="php"){
-           var editor = $('.CodeMirror')[0].CodeMirror;
-           editor.setOption("mode","text/x-php");  
-        }else if(extension=="html"){
-           var editor = $('.CodeMirror')[0].CodeMirror;
-           editor.setOption("mode","text/html");  
-        }
-     
-  return result;
-}
-
-   Template.instructor.events({
-    'click .records' (event){
+Template.instructor.events({
+  'click .records' (event){
     var filename= event.target.innerText;
-        var tu=document.getElementById('valores').innerHTML =" "+filename;
+    var tu=document.getElementById('valores').innerHTML =" "+filename;
     Meteor.call('instructor.find',(err, res) => {
-    if (err) {
-      alert(err);
-    } else {
-        var text = searchs(res, filename);
+      if (err) {
+        alert(err);
+      } else {
+        var text = Editor.search(res, filename);
         var editor = $('.CodeMirror')[0].CodeMirror;
         editor.setValue(text);   
       }
-   });
+    });
   }
+});
 
- });
 
-
-  Template.instructor.events({
-    'click #menuToggler' (event){
-  $('#wrapper').toggleClass('toggled');
-     }
-  });
-
+Template.instructor.events({
+  'click #menuToggler' (event){
+    $('#wrapper').toggleClass('toggled');
+  }
+});
 
 Template.instructor.onRendered( function() {
   this.editor = CodeMirror.fromTextArea( this.find( "#editorcode" ), {
@@ -96,4 +59,3 @@ Template.instructor.onRendered( function() {
      }
   });
 });
-
