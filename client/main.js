46b5faf920c984dd555aea25b3ea5d94ccf93827
Paul Estrada <paul_e_mtz@hotmail.com>
2016-08-31 09:16:40 -0500

    adding login functionality

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+import '../imports/startup/accounts-config.js';
 import '../imports/ui/home.js';
 import '../imports/ui/body.js';
 import '../imports/ui/instructor.js';
--- server/main.js
+++ server/main.js
@@ -1,8 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/documents.js';
+
 Meteor.startup(() => {
  // code to run on server at startup
 
  //Seed users
+ // default instructor
+ var noInstructor = Meteor.users.find({username: "instructor"}).count() == 0;
+ if (noInstructor) Accounts.createUser({username: "instructor", password:"instructor"});
  
 });
